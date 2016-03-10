a88e5ce3daafe824a794f938f6d1ef9e7c771a94
Douglas Ribeiro <douglas.real@gmail.com>
2016-03-10 01:25:17 -0300

    commit inicial

---
+++ app/src/main/java/com/douglasribeiro/mailchimp/MailchimpAutomatizeFreeSyncApplication.java
@@ -0,0 +1,12 @@
+package com.douglasribeiro.mailchimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailchimpAutomatizeFreeSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailchimpAutomatizeFreeSyncApplication.class, args);
+	}
+}
